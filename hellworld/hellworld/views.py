3d34dd4c9a636c4bd711f51a2cb6df4b4e61e8a0
tanxw <txtxw@163.com>
2020-04-21 00:57:08 +0800

    Initial commit

---
+++ hellworld/hellworld/views.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/4/21 0:27
+# @Author  : tanxw
+# from django.shortcuts import render
+#
+#
+# def index(request):
+#     return render(request, 'test/index.html', {'h1': 'hello'})
